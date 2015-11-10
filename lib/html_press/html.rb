7671bb9c4c098a356698c831ffa1419aeeb3272f
Dan Boulet <dan@danboulet.com>
2015-11-10 00:44:23 -0600

    Remove cite from list of block elements
    Fixes bug which causes white space surrounding cite elements to be removed.

--- lib/html_press/html.rb
+++ lib/html_press/html.rb
@@ -123,7 +123,7 @@ module HtmlPress
     # remove whitespaces outside of block elements
     def process_block_elements (out)
       re = '\\s+(<\\/?(?:area|base(?:font)?|blockquote|body' +
-        '|caption|center|cite|col(?:group)?|dd|dir|div|dl|dt|fieldset|form' +
+        '|caption|center|col(?:group)?|dd|dir|div|dl|dt|fieldset|form' +
         '|frame(?:set)?|h[1-6]|head|hr|html|legend|li|link|map|menu|meta' +
         '|ol|opt(?:group|ion)|p|param|t(?:able|body|head|d|h|r|foot|itle)' +
         '|ul)\\b[^>]*>)'
